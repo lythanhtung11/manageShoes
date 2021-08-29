58e543b12f9acdb2ff130f1c84c80ffd40a7b3d1
Thành Đoàn <thanh.funn1999@gmail.com>
2021-08-29 21:22:08 +0700

    test class

---
+++ ProjectWeb/Views/Product/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb.Views.Product
+{
+    public class Class1
+    {
+    }
+}
